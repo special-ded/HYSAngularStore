595bbc519b76e2aa4c9433975de5f1ac678da7b0
Ivan Draganov <draganov.ivan.an@gmail.com>
2022-12-06 21:02:49 +0200

    ls_15 loadChildren: () added

--- store/src/app/app-routing.module.ts
+++ store/src/app/app-routing.module.ts
@@ -1,10 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { ProductsComponent } from './products/products.component';
+import { ProductsRoutingModule } from './products-routing/products-routing.module';
 
 const routes: Routes = [
-  { path: '', redirectTo: '/products', pathMatch: 'full' },
-  { path: 'products', component: ProductsComponent }
+  {
+    path: '',
+    redirectTo: '/products',
+    pathMatch: 'full'
+  },
+  {
+    path: 'products',
+    loadChildren: () => import('./products-routing/products-routing.module').then((m) => ProductsRoutingModule)
+  }
 ];
 
 @NgModule({
