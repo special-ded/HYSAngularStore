5851d7fd558c1ce4fe6ddf34a72a93aaf3300423
Ivan Draganov <draganov.ivan.an@gmail.com>
2022-12-12 17:28:51 +0200

    ls_17 administration component created

--- store/src/app/app.module.ts
+++ store/src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AdministrationComponent } from './administration/administration.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AdministrationComponent
   ],
   imports: [
     BrowserModule,
