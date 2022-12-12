be6a393a1ea2155cd92d0109a2d82e5eaed93661
Ivan Draganov <draganov.ivan.an@gmail.com>
2022-12-12 17:05:28 +0200

    http client deleted

--- store/src/app/app.module.ts
+++ store/src/app/app.module.ts
@@ -1,8 +1,5 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { HttpClientModule } from '@angular/common/http';
-
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
@@ -13,8 +10,7 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule,
-    HttpClientModule
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
