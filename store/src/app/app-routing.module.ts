135391d7d2ad593a9227a44e1ec9fcf348afbf70
Ivan Draganov <draganov.ivan.an@gmail.com>
2022-12-14 11:36:44 +0200

    ls_17 fixed bug

--- store/src/app/app-routing.module.ts
+++ store/src/app/app-routing.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { PageNotFoundComponent } from './products/page-not-found/page-not-found.component';
+import { PageNotFoundComponent } from './shared/components/page-not-found/page-not-found.component';
 
 const routes: Routes = [
   {
